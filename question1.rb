7ad5abbf1c52b4639abf679a85277ef990f005c1
Kleber Pinel <kleberpinel@gmail.com>
2018-09-06 00:05:15 -0700

    Add questions to challenges

--- question1.rb
+++ question1.rb
@@ -1,3 +1,17 @@
+# With the array (1,2,3,4,5,6), write a recursive function that outputs:
+# <1>
+#   <2>
+#     <3>
+#       <4>
+#         <5>
+#           <6>
+#           </6>
+#         </5>
+#       </4>
+#     </3>
+#   </2>
+# </1>
+
 module MOGOInterview
   class Question1
     def self.call(array)
@@ -15,7 +29,7 @@ end
 
 puts MOGOInterview::Question1.call([1, 2, 3, 4, 5, 6])
 
-# Output
+# $ ruby question1.rb
 # <1>
 #   <2>
 #     <3>
